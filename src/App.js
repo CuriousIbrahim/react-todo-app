022a21355e0197452b01ad4f12de7d0da2ffee6b
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-07-09 07:57:51 -0400

    Change render, add state to hold todos as objects, add todos for testing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todos from './Components/Todos';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    }
+  }
+
+  setTodos() {
+    this.state.todos = [
+      {
+        id: 1,
+        name: 'Eat breakfast'
+      }, {
+        id: 2,
+        name: 'Buy groceries'
+      }
+    ]
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+    this.setTodos();
+
+    return (<div className="App">
+
+      <Todos todos={this.state.todos}></Todos>
+    </div>);
   }
 }
 
