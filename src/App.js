a3d0ede33ded9d206092c17e507298cd435682ee
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-07-09 08:21:22 -0400

    Comment out the import of App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Todos from './Components/Todos';
 
 class App extends Component {
