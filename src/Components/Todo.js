0b9befcf0a80d84d460c68d0d83901647af64477
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-07-15 21:08:39 -0400

    <li> now inside of <ReactCSSTransitionGroup>

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './Todo.css';
 
 class Todo extends Component {
@@ -9,10 +10,12 @@ class Todo extends Component {
   }
 
   render() {
-    return (<li onClick={this.delete.bind(this, this.props.id)} class="todo" id={this.props.id}>{this.props.name}
-    </li>)
+    return (<ReactCSSTransitionGroup transitonName="todo-animation" trasnitionEnterTimeout={500} transitionLeaveTimeout={700}>
+      <li onClick={this.delete.bind(this, this.props.id)} class="todo" id={this.props.id}>
+        {this.props.name}
+      </li>
+    </ReactCSSTransitionGroup>)
   }
-
 }
 
 export default Todo;
