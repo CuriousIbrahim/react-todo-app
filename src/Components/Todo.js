3979a539c43e24eb784dd7981ebf85148402fc25
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-07-31 17:40:27 -0400

    Attempt CSSTransitions - does not work, idk why

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,20 +1,29 @@
-import React, {Component} from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import './Todo.css';
+import React, { Component } from "react";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import "./Todo.css";
 
 class Todo extends Component {
-
   delete(id) {
     console.log(id);
     this.props.onDelete(id);
   }
 
   render() {
-    return (<ReactCSSTransitionGroup transitonName="todo-animation" trasnitionEnterTimeout={500} transitionLeaveTimeout={700}>
-      <li onClick={this.delete.bind(this, this.props.id)} class="todo" id={this.props.id}>
-        {this.props.name}
-      </li>
-    </ReactCSSTransitionGroup>)
+    return (
+      <ReactCSSTransitionGroup
+        transitionName="todo-anim"
+        transitionEnterTimeout={700}
+        transitionLeave={700}
+      >
+        <li
+          onClick={this.delete.bind(this, this.props.id)}
+          className="todo"
+          id={this.props.id}
+        >
+          {this.props.name}
+        </li>
+      </ReactCSSTransitionGroup>
+    );
   }
 }
 
