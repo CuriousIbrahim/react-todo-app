f62d931153d59580f8e8d69ec14285c8c4739a91
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2020-01-13 14:13:23 -0500

    Updates

---
+++ precache-manifest.9177031a9edd91eaa619d92350badb28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd417f68a637ffbbbbaa4ded6e41cbc",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "200df42ee77539917289",
+    "url": "/react-todo-app/static/css/main.31445a22.chunk.css"
+  },
+  {
+    "revision": "07986ee5b1a8739f4963",
+    "url": "/react-todo-app/static/js/2.45b2c5fe.chunk.js"
+  },
+  {
+    "revision": "fb433b688e7600a47bea026c80391629",
+    "url": "/react-todo-app/static/js/2.45b2c5fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "200df42ee77539917289",
+    "url": "/react-todo-app/static/js/main.791b8d03.chunk.js"
+  },
+  {
+    "revision": "cfa7889207d55de538f9",
+    "url": "/react-todo-app/static/js/runtime-main.65c34d77.js"
+  }
+]);
